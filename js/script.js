12d3e8b38b679a104de9cf1d0c8fe2ab9fcebc17
Mikelangelo64 <donni2001331gmail.com>
2022-07-17 16:55:55 +0300

    v-2.0

--- js/script.js
+++ js/script.js
@@ -121,6 +121,38 @@ $(document).ready(function(){
         $('body').removeClass('_lock')
     })
 
+    $('.product-section-comments__title__container .product-section-comments__write__comment').click(function(e){
+        e.preventDefault()
+        $('.pop-up-comment').addClass('_avtive-pop-up')
+        $('body').addClass('_lock')
+    })
+
+    $('.pop-up-comment__close__btn').click(function(e){
+        $('.pop-up-comment').removeClass('_avtive-pop-up')
+        $('body').removeClass('_lock')
+    })
+    
+    //pop up raiting
+    let stars = Array.from($('input[name=radio-raiting]'))
+    //console.log(stars);
+
+    $('input[name=radio-raiting]').click(function(e){
+        let currentIndex = stars.indexOf(e.target)
+        //console.log(e.target === stars[0]);
+
+        stars.forEach((item, index, arr) => {
+            if(index <= currentIndex){
+                $(item).next().children('path').css('fill', '#FFC90C')
+            }
+            if(index > currentIndex){
+                $(item).next().children('path').css('fill', '#EFF1F4')
+            }
+        })
+    })
+
+    
+
+
     //blog-anchor
     $('.blog-titles__list .blog-titles__link').click(function(event){
         onBlogLinkClick(event);
@@ -157,6 +189,7 @@ $(document).ready(function(){
 
     $('.for-customer__separate .content-separate__separate__wrapper .for-customer-article').not($('.for-customer__separate .content-separate__separate__wrapper .for-customer-article._active-separate ')).fadeOut(200)
     $('.header-catalog__wrapper .content-separate__separate__wrapper .header-catalog-category').not($('.header-catalog__wrapper .content-separate__separate__wrapper .header-catalog-category._active-separate ')).fadeOut(200)
+    $(' .product-section').not($('.product-section._active-page')).fadeOut(200)
 
     $('.for-customer__separate .content-separate-menu .content-separate-menu__item').click(function(event){
         onCustomerMenuClick.apply(this, [event, 'for-customer__separate', 'for-customer-article']);
@@ -176,6 +209,42 @@ $(document).ready(function(){
         }
     })
 
+    $('.product-pages .product-pages__item').click(function(e){
+        e.preventDefault()
+        const goto = $(this).attr('data-product-page');
+
+        if(goto && $(goto)){
+            $(this).addClass("_active-page");
+            $(goto).addClass("_active-page");
+
+            $(`.product-pages .product-pages__item`).not($(this)).removeClass("_active-page");
+            $(`.product-section`).not($(goto)).removeClass("_active-page");
+            $(`.product-section`).not($(goto)).fadeOut(0)
+        
+            $(goto).fadeIn(400);
+        }
+    })
+
+    $('.comment__action__wrapper .comment__action__item').not($('.comment__action__wrapper .comment__action__item._active-action')).fadeOut(200)
+    
+    
+    $('.product-section__comments .comment__actions a').click(function(e){
+            e.preventDefault()
+
+            const goto = $(this).attr('data-active-action');
+    
+            if(goto && $(goto)){
+                $(this).addClass("_active-action");
+                $(goto).addClass("_active-action");
+    
+                $(`.product-section__comments .comment__actions a`).not($(this)).removeClass("_active-action");
+                $(`.product-section__comments .comment__action__wrapper .comment__action__item`).not($(goto)).removeClass("_active-action");
+                $(`.product-section__comments .comment__action__wrapper .comment__action__item`).not($(goto)).fadeOut(0)
+            
+                $(goto).fadeIn(400);
+            }
+    })
+
     function onCustomerMenuClick(event, section, separateItem){
         //console.log(1, this);
         //const menuLink = event.target;
@@ -184,8 +253,6 @@ $(document).ready(function(){
         if(goto && $(goto)){
             $(this).addClass("_active-separate");
             $(goto).addClass("_active-separate");
-
-            
 
             $(`.${section} .content-separate-menu .content-separate-menu__item`).not($(this)).removeClass("_active-separate");
             $(`.${section} .content-separate__separate__wrapper .${separateItem}`).not($(goto)).removeClass("_active-separate");
@@ -208,6 +275,150 @@ $(document).ready(function(){
         }, 500)
 
     }
+
+
+    //CHECK ANSWER LIST / GALLERY IS EMPTY
+    // $('.answers__list').each(function(){
+    //         if($(this).children().length === 0){
+    //             $('a[data-active-action=.answers]').css('display', 'none')
+    //         }
+
+    //     }
+    // );
+
+    //INPUT-FILE---------------------------------------------------------
+    let inputCounter = 0
+    let inputCounterComment = 0
+
+    
+    
+
+    let filePopUpCompredForm = $( '.pop-up-compred__form input[type=file]' )[0];
+    let fileWriteAnswerForm = $( '.write-answer__form input[type=file]' )[0];
+    let fileCommentForm = $( '.pop-up-comment__form input[type=file]' )[0];
+
+    
+    
+    if(filePopUpCompredForm){
+        inputHandler(filePopUpCompredForm)
+    }
+    if(fileWriteAnswerForm){
+        inputHandler(fileWriteAnswerForm)
+        $('.write-answer__form input[type="file"]')[inputCounter].addEventListener('change', addNewInput);
+        
+        
+    }
+    if(fileCommentForm){
+        inputHandler(fileCommentForm)
+        $('.pop-up-comment__form input[type="file"]')[inputCounter].addEventListener('change', addNewInputForComment);
+    }
+
+    //function input-file handler
+    function inputHandler(input){
+        
+        let label	 = input.nextElementSibling
+        let labelVal = label.innerHTML
+        
+
+        input.addEventListener( 'change', function( e ){
+            //console.log('hui', e.target.files[0].name);
+            let fileName = '';
+            
+            try{
+                fileName = e.target.files[0].name
+            } catch{
+                fileName = ''
+            }
+            
+
+            if( fileName ){
+                label.querySelector( 'span' ).innerHTML = fileName;
+
+
+            }else{
+                label.innerHTML = labelVal;
+            }
+        })
+    }
+
+    //function add new input file
+    function addNewInput(e){
+        inputCounter++
+        console.log(inputCounter);
+        console.log($(e.target));
+        
+
+        $(`
+            <div>
+                <input type="file" name="photo-${inputCounter}" id="photo-${inputCounter}"/>
+
+                <label for="photo-${inputCounter}">
+                    <svg width="19" height="13" viewBox="0 0 19 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M15.2 0.039978H3.8L0 6.11998V6.49996V12.9599H19V6.49996V6.11998L15.2 0.039978ZM4.256 0.799978H14.744L18.088 6.11998H12.92V7.63998C12.92 8.47596 12.236 9.15998 11.4 9.15998H7.6C6.76398 9.15998 6.08 8.47596 6.08 7.63998V6.11998H0.912L4.256 0.799978ZM18.24 12.2H0.76V6.87998H5.32V8.02C5.32 9.084 6.15602 9.91998 7.21998 9.91998H11.78C12.844 9.91998 13.68 9.084 13.68 8.02V6.87998H18.24V12.2Z" fill="black" fill-opacity="0.26"/>
+                    </svg>
+                    <span>
+                        Загрузить фото
+                    </span>
+                </label>            
+            </div>
+        `).insertBefore('.write-answer__form button[type=submit]')
+
+        e.target.removeEventListener('change', addNewInput)
+
+        $( '.write-answer__form input[type=file]' )[inputCounter].addEventListener('change', addNewInput)
+        
+        inputHandler($( '.write-answer__form input[type=file]' )[inputCounter])
+
+
+    }
+    function addNewInputForComment(e){
+        inputCounterComment++
+        // console.log(inputCounterComment);
+        // console.log($(e.target));
+        
+
+        $(`
+            <div>
+                <p></p>
+                <input type="file" name="photo-${inputCounterComment}" id="photo-${inputCounterComment}"/>
+
+                <label for="photo-${inputCounterComment}">
+                    <svg width="19" height="13" viewBox="0 0 19 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M15.2 0.039978H3.8L0 6.11998V6.49996V12.9599H19V6.49996V6.11998L15.2 0.039978ZM4.256 0.799978H14.744L18.088 6.11998H12.92V7.63998C12.92 8.47596 12.236 9.15998 11.4 9.15998H7.6C6.76398 9.15998 6.08 8.47596 6.08 7.63998V6.11998H0.912L4.256 0.799978ZM18.24 12.2H0.76V6.87998H5.32V8.02C5.32 9.084 6.15602 9.91998 7.21998 9.91998H11.78C12.844 9.91998 13.68 9.084 13.68 8.02V6.87998H18.24V12.2Z" fill="black" fill-opacity="0.26"/>
+                    </svg>
+                    <span>
+                        Загрузить фото
+                    </span>
+                </label>            
+            </div>
+        `).insertBefore('.pop-up-comment__form button[type=submit]')
+
+        e.target.removeEventListener('change', addNewInputForComment)
+
+        $( '.pop-up-comment__form input[type=file]' )[inputCounterComment].addEventListener('change', addNewInputForComment)
+        
+        inputHandler($( '.pop-up-comment__form input[type=file]')[inputCounterComment])
+
+
+    }
+
+    //fancybox
+    if(Array.from($('.product-section__comments')).length !== 0){
+        Fancybox.bind('[data-fancybox="gallery"]', {
+            dragToClose: false,
+          
+            Toolbar: false,
+            closeButton: "top",
+          
+            Image: {
+              zoom: false,
+            },
+        })
+    }
+    
+
+
+    
 
     //faq accordion
     $('.faq-accordion .faq-accordion__body').slideUp(300)
